e124d5f1064e918780e8f971cb4fe4468abb8891
deepakviswanadh <deepak16c@gmail.com>
2025-02-10 19:39:39 -0500

    fixed layout

--- src/purecomponents/PureCarousel/CarouselComponent/CarouselComponent.jsx
+++ src/purecomponents/PureCarousel/CarouselComponent/CarouselComponent.jsx
@@ -1,24 +1,14 @@
 import React from "react";
 import classes from "./CarouselComponent.module.css";
-import CarouselHandler from "./CarouselHandlers";
 
-const CarouselComponent = ({ info: { id, src, alt, zi }, triggerChange }) => {
+const CarouselComponent = ({ info: { id, src, alt, zi, content, color } }) => {
   return (
     <>
-      <div className={`${classes.place_header} ${classes.center}`}>
-        {"Title is here"}
-      </div>
       <div
         className={`${classes.carouselItem} ${classes.center}`}
-        style={{ zIndex: zi }}
+        style={{ zIndex: zi, backgroundColor: color }}
       >
-        {id}
-      </div>
-      <div className={`${classes.greater_than} ${classes.center}`}>
-        <CarouselHandler type={">"} triggerChange={triggerChange} />
-      </div>
-      <div className={`${classes.less_than} ${classes.center}`}>
-        <CarouselHandler type={"<"} triggerChange={triggerChange} />
+        {content}
       </div>
     </>
   );
--- src/purecomponents/PureCarousel/PureCarousel.jsx
+++ src/purecomponents/PureCarousel/PureCarousel.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import classes from "./PureCarousel.module.css";
 import CarouselComponent from "./CarouselComponent";
+import CarouselHandler from "./CarouselComponent/CarouselHandlers";
 
 const PureCarousel = () => {
   const [data, setData] = useState([
-    { id: 1, src: "", alt: "", zi: -1 },
-    { id: 2, src: "", alt: "", zi: 0 },
-    { id: 3, src: "", alt: "", zi: 1 },
+    {
+      id: 1,
+      src: "",
+      alt: "",
+      zi: -1,
+      content: "slide1",
+      color: "lightYellow",
+    },
+    { id: 2, src: "", alt: "", zi: 0, content: "slide2", color: "lightPink" },
+    { id: 3, src: "", alt: "", zi: 1, content: "slide3", color: "lightGrey" },
   ]);
 
   const triggerChange = (flag) => {
@@ -30,16 +38,20 @@ const PureCarousel = () => {
     <div className={classes.main_container}>
       <div className={classes.carouselWrapper}>
         {data.map((each) => (
-          <CarouselComponent
-            info={each}
-            key={each.id}
-            triggerChange={triggerChange}
-          />
+          <CarouselComponent info={each} key={each.id} />
         ))}
       </div>
 
+      <div className={`${classes.greater_than} ${classes.center}`}>
+        <CarouselHandler type={">"} triggerChange={triggerChange} />
+      </div>
+
+      <div className={`${classes.less_than} ${classes.center}`}>
+        <CarouselHandler type={"<"} triggerChange={triggerChange} />
+      </div>
+
       <div className={`${classes.dot_container} ${classes.center}`}>
-        <h2>dots here please</h2>
+        <h3>dots here please</h3>
       </div>
     </div>
   );
